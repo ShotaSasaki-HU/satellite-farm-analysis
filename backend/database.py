14d0b56716479a57d1edf9dfb853d59d0e84721c
ShotaSasaki-HU <invisiblefrontline@gmail.com>
2025-05-06 12:12:57 +0900

    DB関連の引数調整

--- backend/database.py
+++ backend/database.py
@@ -4,11 +4,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # SQLAlchemyがDBと接続するための窓口であるengineを作る。
 # aoi.dbがなければ自動作成
-DATABASE_URL = "sqlite:///./aoi.db"
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "sqlite:///./agri_eye.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# connect_argsは、SQLite用の引数らしい。詳細はのちのち。
 
 # セッションを作成する準備
-SessionLocal = sessionmaker(bind=engine) # セッションを作るクラスを作成
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False) # セッションを作るクラスを作成
 # 実際にセッションを作るのはmain.py
 
 # モデルクラスを作る。（テーブル定義を書く。）
